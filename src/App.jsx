5b70a26ce224276b5e967bfbe14873bd17845d52
yasirukodithuwakku <143882035+yasirukodithuwakku@users.noreply.github.com>
2024-12-24 16:50:52 +0530

    Changed Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import EnergyAuditDashboard from '../src/components/EnergyAuditDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <EnergyAuditDashboard/>
       )
